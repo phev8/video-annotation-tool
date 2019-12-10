838920a13151f4a3627aec97a1a3cc25bb308613
unknown <sajeeth.sadanand@gmail.com>
2019-12-10 17:48:23 +0530

    Rolling back separation of template for timeline

--- web/src/app/editor/timeline/timeline.component.ts
+++ web/src/app/editor/timeline/timeline.component.ts
@@ -9,7 +9,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import * as vis from 'vis';
-import { DataGroup, DataItem, DataSelectionOptions, IdType, Timeline, TimelineOptions } from 'vis';
+import { DataGroup, DataItem, IdType, Timeline, TimelineOptions } from 'vis';
 import * as moment from 'moment';
 import { LabelsService } from 'src/app/labels/labels.service';
 import { CurrentProjectService } from '../current-project.service';
@@ -21,13 +21,9 @@ import { ProjectModel } from '../../models/project.model';
 import { Time } from './time';
 import { TimelineData } from './timeline.data';
 import _ from "lodash";
-import { LabelModel } from '../../models/label.model';
 import { pairwise, startWith } from 'rxjs/operators';
 import { LabelCategoryModel } from '../../models/labelcategory.model';
-import { element } from 'protractor';
 import { CurrentToolService } from '../project-toolbox.service';
-import { options } from './timeline.template.options';
-import { forEach } from 'typescript-collections/dist/lib/arrays';
 
 @Component({
   selector: 'app-timeline',
@@ -40,9 +36,131 @@ export class TimelineComponent implements OnInit, AfterViewInit, OnDestroy {
 
   loading = true;
   private project: ProjectModel;
+
+
   private timeline: Timeline;
 
   @Output() showToolBox = new EventEmitter<boolean>();
+
+  // noinspection SpellCheckingInspection
+  // noinspection JSUnusedGlobalSymbols
+  private options = {
+    width: '100%',
+    min: Time.seconds(0),
+    start: Time.seconds(0),
+    end: Time.seconds(15),
+    max: Time.seconds(98), // todo
+    moment: function (date) {
+      return moment(date).utc();
+    },
+    format: {
+      minorLabels: {
+        millisecond: 'ss.SSS',
+        second: 'HH:mm:ss',
+        minute: 'HH:mm:ss',
+        hour: 'HH:mm',
+        weekday: 'HH:mm',
+        day: 'HH:mm',
+        week: 'HH:mm',
+        month: 'HH:mm',
+        year: 'HH:mm'
+      },
+      majorLabels: {
+        second: 'HH:mm:ss',
+        minute: 'HH:mm:ss',
+        hour: '',
+        weekday: '',
+        day: '',
+        week: '',
+        month: '',
+        year: ''
+      }
+    },
+    groupTemplate: (group: DataGroup) => {
+      if (group) {
+
+        const overarchingContainer = document.createElement('div');
+        overarchingContainer.className = 'clr-row top-margin';
+
+        const containerLeft = document.createElement('div');
+        containerLeft.className = 'clr-col-6';
+
+        const containerRight = document.createElement('div');
+        containerRight.className = 'clr-col-6';
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.className = 'btn btn-link';
+
+        const categoryLabel = document.createElement('label');
+        categoryLabel.innerHTML = group['category'];
+        categoryLabel.addEventListener('click', () => {
+          this.labelsService.addLabel(JSON.parse(localStorage.getItem('currentSession$'))['user']['id'],group['categoryId'], this.userRole);
+        });
+
+        categoryContainer.append(categoryLabel);
+
+        const container = document.createElement('div');
+        container.className = 'checkbox btn';
+
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = `checkbox_${group.id}`;
+        input.addEventListener('change', () => {
+          this.checkForTracking(group['categoryId']);
+          this.checkboxChange.emit({id: group.id, checked: input.checked});
+        });
+
+        const label = document.createElement('label');
+        label.setAttribute('style', 'padding-right: 2em;');
+        label.setAttribute('for', `checkbox_${group.id}`);
+        label.innerHTML = group.content;
+
+        container.prepend(input);
+        container.append(label);
+
+        containerLeft.appendChild(categoryContainer);
+        containerRight.appendChild(container);
+
+        overarchingContainer.appendChild(containerLeft);
+        overarchingContainer.appendChild(containerRight);
+        return overarchingContainer;
+      }
+    },
+    multiselect: true,
+    editable: true,
+    onAdd: (item, callback) => {
+      console.log('onAdd', item);
+      item.content = 'tracker';
+      callback(item); // send back adjusted new item
+    },
+    onMove: (item, callback) => {
+      console.log('onMove', item);
+      callback(item);
+    },
+    onMoving: (item, callback) => {
+      console.log('onMoving', item);
+      callback(item);
+    },
+    onUpdate: (item, callback) => {
+      console.log('onUpdate', item);
+      callback(item);
+    },
+    onRemove: (item, callback) => {
+      console.log('onRemove', item);
+      callback(item);
+    },
+    tooltipOnItemUpdateTime: {
+      template: item => {
+        const fstart = Time.formatDatetime(item.start);
+        const fend = Time.formatDatetime(item.end);
+        return `Start: ${fstart}<br>End:${fend}`;
+      }
+    },
+    tooltip: {
+      followMouse: true,
+      overflowMethod: 'cap'
+    }
+  };
 
   private timelineData: TimelineData = new TimelineData();
   private labelCategories: LabelCategoryModel[] = [];
@@ -50,6 +168,7 @@ export class TimelineComponent implements OnInit, AfterViewInit, OnDestroy {
   private subscription: Subscription;
   private updateSubscription: Subscription;
   private currentTime = 0;
+
   private checkboxChange = new EventEmitter<{ id: IdType, checked: boolean }>();
   private userRole: string;
 
@@ -175,7 +294,7 @@ export class TimelineComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     const container = this.timelineVisualization.nativeElement;
     // @ts-ignore
-    this.timeline = new vis.Timeline(container, this.timelineData.items, this.timelineData.groups, options);
+    this.timeline = new vis.Timeline(container, this.timelineData.items, this.timelineData.groups, this.options);
     this.customTimeId = this.timeline.addCustomTime(Time.seconds(1), 'currentPlayingTime');
     this.timeline.setCustomTimeTitle('seeker', this.customTimeId);
 
@@ -340,7 +459,7 @@ export class TimelineComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private setMax(duration: number) {
     // @ts-ignore
-    const newOptions: TimelineOptions = Object.assign({}, options);
+    const newOptions: TimelineOptions = Object.assign({}, this.options);
     newOptions.max = duration;
     this.timeline.setOptions(newOptions);
   }
